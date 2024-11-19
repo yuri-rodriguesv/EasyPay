15cd19c5429c798d0a8a25a7908eff9a8331ee02
Yuri Rodrigues <yuri_rodrigues2017@outlook.com>
2024-11-19 00:08:18 -0300

    adicao de links entre os topicos

--- easypays/urls.py
+++ easypays/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics', views.topics, name='topics'),
+    path('topics/<topic_id>/', views.topic, name='topic'),
 ]
