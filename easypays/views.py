a8c5c6bf2f679c904d2f687c7a4986d9844cbcc7
Yuri Rodrigues <yuri_rodrigues2017@outlook.com>
2024-11-18 22:27:47 -0300

    Adicao de modal de topics

--- easypays/views.py
+++ easypays/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
-
+from .models import Topic
 def index(request):
     return render(request, 'easypays/index.html')
+
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added') 
+    context = {'topic': topics}
+    return render(request, 'easypays/topics.html',context)
