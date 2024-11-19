fbd5d3a0fdf98ca71c3a2bf5ef0cd88aa557bc72
Yuri Rodrigues <yuri_rodrigues2017@outlook.com>
2024-11-19 15:38:14 -0300

    finalização do app easypays

--- easypays/urls.py
+++ easypays/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('topics', views.topics, name='topics'),
     path('topics/<topic_id>/', views.topic, name='topic'),
     path('new_topic', views.new_topic, name='new_topic'),
+    path('new_entry/<topic_id>', views.new_entry, name='new_entry'),
+    path('edit_entry/<entry_id>', views.edit_entry, name='edit_entry'),
 ]
--- easypays/views.py
+++ easypays/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Topic
-from .forms import TopicForm
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 def index(request):
@@ -35,3 +35,35 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'easypays/new_topic.html', context)
 
+def new_entry(request,topic_id):
+    """Create a new entry for a topic"""
+    topic = Topic.objects.get(id=topic_id)
+    form = EntryForm(request.POST)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        # Dados de POST submetidos; processa os dados
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'easypays/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic.id]))
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'easypays/edit_entry.html', context)
