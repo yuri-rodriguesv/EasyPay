fbbe5a2153c210af379b4c65d1868e51c4db66c6
Yuri Rodrigues <yuri_rodrigues2017@outlook.com>
2024-11-20 01:48:54 -0300

    adicao login logout e register

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+urlpatterns = [
+    path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('register', views.register, name='register'),
+]
